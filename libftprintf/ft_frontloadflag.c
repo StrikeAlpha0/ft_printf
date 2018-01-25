4566e935ff62451549a46a1942b08b85142765ac
Maurice Sharpe <msharpe@e1z2r9p27.42.us.org>
2018-01-25 15:18:26 -0800

    Testing to see if segfaut is midload

--- libftprintf/ft_frontloadflag.c
+++ libftprintf/ft_frontloadflag.c
@@ -6,7 +6,7 @@
 /*   By: msharpe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/22 15:27:37 by msharpe           #+#    #+#             */
-/*   Updated: 2018/01/25 14:44:08 by msharpe          ###   ########.fr       */
+/*   Updated: 2018/01/25 15:16:16 by msharpe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,11 @@ void			ft_frontloadflag(long n, t_inputinfo *info, t_passinfo *pass)
 			ft_flag_space(n, info, pass);
 			info->f++;
 		}
-		if (info->flag[info->f] == '0' || info->flag[info->f] == '.')
-		{
-			ft_flag_zero(n, info, pass);
-			info->f++;
-		}
+	//	if (info->flag[info->f] == '0' || info->flag[info->f] == '.')
+	//	{
+	//		ft_flag_zero(n, info, pass);
+	//		info->f++;
+	//	}
 	}
 	if (info->flag[info->f] == '\0' && pass->numlen != pass->width)
 	{
