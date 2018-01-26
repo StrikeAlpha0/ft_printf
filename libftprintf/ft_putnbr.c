3abe85a54dbe5cd261ff1aa47a5eedd275b1f588
Maurice Sharpe <msharpe@e1z2r9p27.42.us.org>
2018-01-25 22:17:15 -0800

    May fix spacing issue in minus flag

--- libftprintf/ft_putnbr.c
+++ libftprintf/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: msharpe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 19:29:01 by msharpe           #+#    #+#             */
-/*   Updated: 2018/01/25 21:27:10 by msharpe          ###   ########.fr       */
+/*   Updated: 2018/01/25 22:12:40 by msharpe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@
 void		ft_putnbr(long n, t_inputinfo *info, t_passinfo *pass)
 {
 	info->f = 0;
-	if (n < 0)
-		pass->numlen++;
 	ft_frontloadflag(n, info, pass);
 	if (n < 0)
+	{
 		n = -n;
+		pass->numlen++;
+	}
 	//ft_midloadflag(n, info, pass);
 	ft_putnbrup(n);
 	if (info->flag[info->f == '-'])
