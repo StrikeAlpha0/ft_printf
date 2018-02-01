c38b6d75b2c555737c4c719f97b2471ef151a9c3
Maurice Sharpe <msharpe@e1z2r9p27.42.us.org>
2018-01-31 21:41:38 -0800

    fixed non-existant tables in ft_putstr

--- libftprintf/ft_putstr.c
+++ libftprintf/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: msharpe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 22:42:31 by msharpe           #+#    #+#             */
-/*   Updated: 2018/01/31 21:25:32 by msharpe          ###   ########.fr       */
+/*   Updated: 2018/01/31 21:40:24 by msharpe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	ft_putstr(char const *s, t_inputinfo *info, t_passinfo *pass)
 	info->f = 0;
 	if (s == NULL)
 		 write(1, "(null)", 6);
-	while (info->flag[info->f].name != '.' && info->flag[info->f].name != '\0')
+	while (info->flag[info->f] != '.' && info->flag[info->f] != '\0')
 		info->f++;
-	if (info->flag[info->f].name == '.' && info->flag[info->f].name !='\0')
+	if (info->flag[info->f] == '.' && info->flag[info->f] !='\0')
 	{
 		info->f++;
 		pass->precision = ft_atoi(info->flag + info->f);
