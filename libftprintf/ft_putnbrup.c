2d3d3cc03ee58471e6bb55a13da780fa380be28c
Maurice Sharpe <msharpe@e1z2r9p27.42.us.org>
2018-02-02 13:19:25 -0800

    Reset the unsigned flags

--- libftprintf/ft_putnbrup.c
+++ libftprintf/ft_putnbrup.c
@@ -6,7 +6,7 @@
 /*   By: msharpe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 23:20:56 by msharpe           #+#    #+#             */
-/*   Updated: 2018/02/02 13:02:34 by msharpe          ###   ########.fr       */
+/*   Updated: 2018/02/02 13:18:14 by msharpe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,8 @@
 
 void		ft_putnbrup(unsigned long n, t_inputinfo *info, t_passinfo *pass)
 {
-	if (info->z == 0)
-	{
-		ft_frontloadflag(n, info, pass);
-		ft_midloadflag(n, info, pass);
-	}
 	if (n > 9)
 	{
-		if (info->z == 0)
-			info->z = 2;
 		ft_putnbrup(n / 10, info, pass);
 		ft_putnbrup(n % 10, info, pass);
 	}
@@ -30,10 +23,5 @@ void		ft_putnbrup(unsigned long n, t_inputinfo *info, t_passinfo *pass)
 	{
 		ft_putchar(n + '0');
 		pass->final_count++;
-	}
-	if (info->z == 2)
-	{
-		ft_backloadflag(n, info, pass);
-		info->z = 3;
 	}
 }
