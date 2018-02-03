dd3b4f950d1828575b5205ad31b2d13e1d732753
Maurice Sharpe <msharpe@e1z2r9p27.42.us.org>
2018-02-02 23:27:35 -0800

    May have fixed the S flag. Not sure

--- libftprintf/printfwriter.c
+++ libftprintf/printfwriter.c
@@ -6,7 +6,7 @@
 /*   By: msharpe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/16 23:09:03 by msharpe           #+#    #+#             */
-/*   Updated: 2018/02/02 23:05:37 by msharpe          ###   ########.fr       */
+/*   Updated: 2018/02/02 23:25:03 by msharpe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,14 @@ void		ft_per_supr(va_list *list, t_inputinfo *info, t_passinfo *pass)
 {
 	pass->numlen = -1;
 	ft_putstrup((char *)va_arg(*list, wchar_t *), info, pass);
+}
+
+void		ft_checkpoint(va_list *list, t_inputinfo *info, t_passinfo *pass)
+{
+	int q;
+
+	if ((q = ft_strstr((info->flag), "l")) && q == 1)
+		ft_wput_strup(va_arg(*list, wchar_t *));
+	else	
+		ft_per_supr(list, info, pass);
 }
