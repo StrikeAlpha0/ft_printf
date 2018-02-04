54ffb5abf0334ce25a38f09a7bcc3812683e5dd5
Maurice Sharpe <msharpe@e1z2r9p27.42.us.org>
2018-02-04 01:56:34 -0800

    Back more

--- libftprintf/ft_putnbr.c
+++ libftprintf/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: msharpe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 19:29:01 by msharpe           #+#    #+#             */
-/*   Updated: 2018/02/04 01:55:32 by msharpe          ###   ########.fr       */
+/*   Updated: 2018/02/04 01:56:20 by msharpe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ void		ft_putnbr(long n, t_inputinfo *info, t_passinfo *pass)
   		info->f++;
  	if (info->flag[info->f] == '\0' && pass->numlen < pass->width && pass->width > info->precision)
   	{
-  		ft_flag_zero(n, info, pass);
+  		info->swi = 2;
+  		ft_flag_space(n, info, pass);
   	}
  	else if (info->flag[info->f] == '\0' && pass->numlen < pass->width && info->precision > pass->width)
  	{
