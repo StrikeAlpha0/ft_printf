c9623fc69256695abf17309c499a3bf1c2d72d94
Maurice Sharpe <msharpe@e1z2r9p27.42.us.org>
2018-02-04 01:48:53 -0800

    Think that the first two numbered tests are good.

--- libftprintf/ft_putnbr.c
+++ libftprintf/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: msharpe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 19:29:01 by msharpe           #+#    #+#             */
-/*   Updated: 2018/02/04 01:37:29 by msharpe          ###   ########.fr       */
+/*   Updated: 2018/02/04 01:48:11 by msharpe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,11 @@ void		ft_putnbr(long n, t_inputinfo *info, t_passinfo *pass)
   	while (info->flag[info->f] != '-' && info->flag[info->f] != '\0' &&
   			info->flag[info->f] != '0')
   		info->f++;
+	if (info->precision > pass->width)
+		pass->width = info->precision;
  	if (info->flag[info->f] == '\0' && pass->numlen < pass->width && pass->width > info->precision)
   	{
-  		info->swi = 2;
-  		ft_flag_space(n, info, pass);
+  		ft_flag_zero(n, info, pass);
   	}
  	else if (info->flag[info->f] == '\0' && pass->numlen < pass->width && info->precision > pass->width)
  	{
