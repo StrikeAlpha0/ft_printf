8e93d5275a0ce5cf11a87e2bdc54b005a8db2319
Maurice Sharpe <msharpe@e1z2r9p27.42.us.org>
2018-02-04 01:14:22 -0800

    Come onnnnnn there has to be something off...

--- libftprintf/ft_putnbr.c
+++ libftprintf/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: msharpe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 19:29:01 by msharpe           #+#    #+#             */
-/*   Updated: 2018/02/04 01:11:53 by msharpe          ###   ########.fr       */
+/*   Updated: 2018/02/04 01:13:56 by msharpe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ void		ft_putnbr(long n, t_inputinfo *info, t_passinfo *pass)
 	info->f = 0;
 	info->z = 1;
 	ft_frontloadflag(n, info, pass);
-//	printf("OOOH NUMLEN:%d\n", pass->numlen);
 	info->f = 0;
 	while (info->flag[info->f] != '-' && info->flag[info->f] != '\0' &&
 			info->flag[info->f] != '0')
@@ -39,7 +38,7 @@ void		ft_putnbr(long n, t_inputinfo *info, t_passinfo *pass)
 			q--;
 		}
 //		pass->width = info->precision;
-//		printf("A: The value of pass->width is %d\n Numlen = %d", pass->width, pass->numlen);
+//		printf("A: The value of pass->width is %d", pass->width);
 		ft_flag_zero(n, info, pass);
 	}
 	else if (info->flag[info->f] == '\0' && pass->numlen < pass->width && info->precision == pass->width)
@@ -49,9 +48,9 @@ void		ft_putnbr(long n, t_inputinfo *info, t_passinfo *pass)
 		ft_flag_zero(n, info, pass);
 //		printf("B");
 	}
-	/*else if (info->flag[info->f] == '\0' && pass->numlen < pass->width && q > 0)
+	else if (info->flag[info->f] == '\0' && pass->numlen < pass->width && q > 0)
 	{
-		printf("C DEEZ The value of q is:%d", q);
+//		printf("C DEEZ The value of q is:%d", q);
 		while (q > 0)
 		{
 			ft_putchar(' ');
@@ -61,7 +60,7 @@ void		ft_putnbr(long n, t_inputinfo *info, t_passinfo *pass)
 		pass->width = info->precision;
 		pass->numlen = pass->numlen - 1;
 
-	}*/
+	}
 	ft_midloadflag(n, info, pass);
 	if (n < 0)
 		n = -n;
