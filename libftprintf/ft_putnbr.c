1c2065d715b57f9c5572d0edceb4120e97bebbd8
Maurice Sharpe <msharpe@e1z2r9p27.42.us.org>
2018-02-03 22:24:31 -0800

    not sure how I feel about this. there might be something wrong with this test.

--- libftprintf/ft_putnbr.c
+++ libftprintf/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: msharpe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 19:29:01 by msharpe           #+#    #+#             */
-/*   Updated: 2018/02/03 22:21:42 by msharpe          ###   ########.fr       */
+/*   Updated: 2018/02/03 22:23:26 by msharpe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,8 @@ void		ft_putnbr(long n, t_inputinfo *info, t_passinfo *pass)
 	}
 	else if (info->flag[info->f] == '\0' && pass->numlen < pass->width && info->precision > pass->width)
 	{
-	//	pass->width = info->precision;
+		pass->width = info->precision;
+		pass->numlen = pass->numlen - 1;
 		ft_flag_zero(n, info, pass);
 	}
 	ft_midloadflag(n, info, pass);
